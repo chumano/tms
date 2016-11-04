148995fe468e931259c786a0536b7d3c620e079b
chumano <xuanloc_hoang2000@yahoo.com>
2016-11-04 10:28:38 +0700

    ok

---
+++ TMS/_Common/SessionCollection.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMS
+{
+    public static class SessionCollection
+    {
+        public static bool IsLogIn
+        {
+            get
+            {
+                return HttpContext.Current.Session["IsLogIn"] == null ? false : (bool)HttpContext.Current.Session["IsLogIn"];
+            }
+            set
+            {
+                HttpContext.Current.Session["IsLogIn"] = value;
+            }
+        }
+
+        public static bool IsLogOut
+        {
+            get
+            {
+                return HttpContext.Current.Session["IsLogOut"] == null ? false : (bool)HttpContext.Current.Session["IsLogOut"];
+            }
+            set
+            {
+                HttpContext.Current.Session["IsLogOut"] = value;
+            }
+        }
+
+        public static int CurrentUserId
+        {
+            get
+            {
+                return HttpContext.Current.Session["CurrentUserId"] != null ? (int)HttpContext.Current.Session["CurrentUserId"] : -1;
+            }
+            set
+            {
+                HttpContext.Current.Session["CurrentUserId"] = value;
+            }
+        }
+
+        public static string UserName
+        {
+            get
+            {
+                return (string)HttpContext.Current.Session["UserName"];
+            }
+            set
+            {
+                HttpContext.Current.Session["UserName"] = value;
+            }
+        }
+
+        //======================================================
+        public static void ClearSession()
+        {
+            HttpContext.Current.Session.Clear();
+        }
+        //======================================================
+        
+
+        public static Dictionary<string, object> ExportObjectData
+        {
+            get
+            {
+                return (Dictionary<string, object>)HttpContext.Current.Session["ExportObjectData"];
+            }
+            set
+            {
+                HttpContext.Current.Session["ExportObjectData"] = value;
+            }
+        }
+
+        public static string ExportTemplate
+        {
+            get
+            {
+                return (string)HttpContext.Current.Session["ExportTemplate"];
+            }
+            set
+            {
+                HttpContext.Current.Session["ExportTemplate"] = value;
+            }
+        }
+
+
+        //======================================================
+
+        public static string DefaultAction
+        {
+            get
+            {
+                return (string)HttpContext.Current.Session["DefaultAction"];
+            }
+            set
+            {
+                HttpContext.Current.Session["DefaultAction"] = value;
+            }
+        }
+
+        public static string DefaultController
+        {
+            get
+            {
+                return (string)HttpContext.Current.Session["DefaultController"];
+            }
+            set
+            {
+                HttpContext.Current.Session["DefaultController"] = value;
+            }
+        }
+
+        public static string LastUrl
+        {
+            get
+            {
+                return (string)HttpContext.Current.Session["LastUrl"];
+            }
+            set
+            {
+                HttpContext.Current.Session["LastUrl"] = value;
+            }
+        }
+
+        public static bool IsDeveloper
+        {
+            get
+            {
+                return HttpContext.Current.Session["IsDeveloper"] != null ? (bool)HttpContext.Current.Session["IsDeveloper"] : false;
+            }
+            set
+            {
+                HttpContext.Current.Session["IsDeveloper"] = value;
+            }
+        }
+        //======================================================
+    }
+}
